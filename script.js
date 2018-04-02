da9dd470a1b6593d98bbfedcebb42a01cf199276
Sniedze <u.sniedze@gmail.com>
2018-04-02 21:34:08 +0200

    creates scroll function for desctop icons

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
-const video = document.getElementById("contest-video");
-const iconsInstructions = document.querySelectorAll("#instructions path, #instructions rect, #instructions circle, #instructions line");
-const iconsFooter = document.querySelectorAll("#footer path, #footer rect, #footer circle, #footer line");
+"use strict";
 
 
 /*Event listener for the video in the index page*/
-
+const video = document.getElementById("contest-video");
 
 video.addEventListener('ended', function () {
     video.currentTime = 0;
     video.load();
 });
 
+/*Function to start drawing icons at the certain scroll point*/
+
+const iconsInstructions = document.querySelectorAll("#instructions path, #instructions rect, #instructions circle, #instructions line");
+const iconsFooter = document.querySelectorAll("#footer path, #footer rect, #footer circle, #footer line");
+const mobileFunction = window.matchMedia("(max-width: 666px)")
 
 window.onscroll = function () {
-    drawsTaskIcons();
-    drawsFooterIcons();
-};
+    if (mobileFunction.matches) {
+
+        drawsTaskIcons();
+        drawsFooterIcons();
+    } else {
+        desctopTaskIcons();
+        desctopFooterIcons();
+    }
+}
 
 function drawsTaskIcons() {
 
@@ -33,7 +42,7 @@ function drawsTaskIcons() {
 
 function drawsFooterIcons() {
 
-    if (document.body.scrollTop > 1200 || document.documentElement.scrollTop > 1200) {
+    if (document.body.scrollTop > 1280 || document.documentElement.scrollTop > 1280) {
         iconsFooter.forEach(e => {
             e.classList.add("drawing");
 
@@ -45,5 +54,63 @@ function drawsFooterIcons() {
     }
 }
 
-var d = document.getElementById("first-svg");
-var topPos = d.offsetTop;
+function desctopTaskIcons() {
+
+    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+        iconsInstructions.forEach(e => {
+            e.classList.add("drawing");
+
+        })
+    } else {
+        iconsInstructions.forEach(e => {
+            e.classList.remove("drawing");
+        })
+    }
+}
+
+function desctopFooterIcons() {
+
+    if (document.body.scrollTop > 1280 || document.documentElement.scrollTop > 1280) {
+        iconsFooter.forEach(e => {
+            e.classList.add("drawing");
+
+        })
+    } else {
+        iconsFooter.forEach(e => {
+            e.classList.remove("drawing");
+        })
+    }
+}
+
+
+/*////////////////////////////MOBILE BURGER MENU ////////////////////*/
+const burger = document.querySelector(".burger");
+const menuList = document.querySelector(".menuList");
+const xButton = document.querySelector(".XButton");
+const menuListItem = document.querySelectorAll(".liItem");
+burger.addEventListener('click', function () {
+    for (var i = 0; i < menuListItem.length; ++i) {
+        menuListItem[i].classList.remove("dontDisplay");
+    }
+    menuList.classList.remove("dontDisplay");
+    menuList.classList.add("menuSlide");
+    burger.classList.add("dontDisplay");
+    xButton.classList.remove("dontDisplay");
+    menuList.addEventListener("animationend", function openMenu() {
+        menuList.removeEventListener("animationend", openMenu);
+    })
+})
+xButton.addEventListener('click', function () {
+    for (var i = 0; i < menuListItem.length; ++i) {
+        menuListItem[i].classList.add("dontDisplay");
+    }
+    xButton.classList.add("dontDisplay");
+    menuList.classList.replace("menuSlide", "menuSlideClose");
+    burger.classList.remove("dontDisplay");
+    menuList.addEventListener("animationend", function closeMenu() {
+        menuList.classList.remove("menuSlideClose");
+        burger.classList.remove("dontDisplay");
+        menuList.removeEventListener("animationend", closeMenu);
+    })
+})
+/////////////////////////////////////////////////////////////
